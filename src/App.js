820a44ea7deac37622fd9494448cd30c0041c765
Paige Boyer <paige1381@gmail.com>
2019-01-16 23:52:52 -0800

    add admin

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import axios from "axios";
+import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      data: [],
+      missingEmails: 0
+    };
+    this.calculateMissingEmails = this.calculateMissingEmails.bind(this);
+  }
+
+  componentWillMount() {
+    axios
+      .get("https://georgia-maxwell-backend.herokuapp.com/invites")
+      .then(res => {
+        this.setState(
+          {
+            data: res.data
+          },
+          () => {
+            this.calculateMissingEmails();
+          }
+        );
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
+  calculateMissingEmails() {
+    let missingEmails = 0;
+    this.state.data.map(item => {
+      if (!item.email) {
+        missingEmails++;
+      }
+      return missingEmails;
+    });
+    this.setState({
+      missingEmails
+    });
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Yay Marriage!</h1>
+        <h4>
+          Before you is the full and unabridged version of Georgia and Maxwell's
+          guest list, enjoy.
+        </h4>
+        <h5>Currently waiting for {this.state.missingEmails} emails</h5>
+        <table>
+          <tr>
+            <th>Guests</th>
+            <th>Email</th>
+          </tr>
+          {this.state.data.map(item => {
+            return (
+              <tr>
+                <td className={!item.email ? "no-email" : ""}>{item.guests}</td>
+                <td>{item.email}</td>
+              </tr>
+            );
+          })}
+        </table>
       </div>
     );
   }
